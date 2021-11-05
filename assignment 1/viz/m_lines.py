9085f53bb01a470bf2ea38d91a886a5abb2922f0
asantos-6 <andresantos_891@hotmail.com>
2021-11-05 16:11:30 +0100

    Lin. Reg. plots for different number of exams with x=percentage

--- assignment 1/viz/m_lines.py
+++ assignment 1/viz/m_lines.py
@@ -23,16 +23,31 @@ mean_d
 df_groups = df.groupby(["Percentage", "Exams"]).mean().reset_index()
 # prob_1 = df_groups[(df_groups.Percentage == 1)]
 
+# p_class = df.loc[:, ["Exams"]].applymap(lambda x: str(x))
+# p_index = p_class.applymap(lambda x: x in ["6", "9", "12", "15", "18"])["Exams"]
+
+#df['Time_code1'] = df["Time_code1"].apply(lambda y: math.log(y * (10 ** 6)))
+# grid = sns.lmplot(data=df.loc[p_index], x="Percentage", y="Time_code1", hue="Exams")
+# grid = grid.set_axis_labels("Percentage", "log(log(Time_code1*10^6))")
+#
+# plt.show()
+#
+# df['Time_code1'] = df["Time_code1"].apply(lambda y: math.log(y * (10 ** 6)))
+# grid = sns.lmplot(data=df.loc[p_index], x="Percentage", y="Time_code1", hue="Exams")
+# grid = grid.set_axis_labels("Percentage", "log(log(Time_code1*10^6))")
+#
+# plt.show()
+
 # Plot Linear Regression
+p_class = df.loc[:, ["Exams"]].applymap(lambda x: str(x))
+time_code = "Time_code2"
+# df[time_code] = df[time_code].apply(lambda y: math.log(y * (10 ** 6)))
+# df[time_code] = df[time_code].apply(lambda y: math.log(y * (10 ** 6)))
+for e in range(6, 16, 1):
 
-p_class = df.loc[:, ["Percentage"]].applymap(lambda x: str(x))
-df['Time_code1'] = df["Time_code1"].apply(lambda y: math.log(y * (10 ** 6)))
-p_index = p_class.applymap(lambda x: x in ["0.0", "0.25", "0.5", "0.75", "0.9", "1.0"])["Percentage"]
-# p_index = p_class.applymap(lambda x: x in ["0.75"])["Percentage"]
+    p_index = p_class.applymap(lambda x: x in [str(e)])["Exams"]
 
-grid = sns.lmplot(data=df.loc[p_index], x="Exams", y="Time_code1", hue="Percentage")
-# grid = sns.regplot(data=df.loc[p_index], x="Exams", y="Time_code1")
-# grid.set(yscale="log")
-grid = grid.set_axis_labels("Exams", "log(Time_code1*10^6)")
+    grid = sns.regplot(data=df.loc[p_index], x="Percentage", y=time_code, line_kws={"color": "red"})
+    grid.set(ylabel=f"log({time_code}*10^6)", title=f"Exams = {str(e)}")
 
-plt.show()
+    plt.show()
